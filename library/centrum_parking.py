47870f0c8f74caf078c1fbc803f20d482e67430d
Bo Henriksen <bomarconi@gmail.com>
2018-11-04 19:25:55 +0100

    Fix assignment 1 and 3.

--- library/centrum_parking.py
+++ library/centrum_parking.py
@@ -2,6 +2,16 @@ import pandas as pd
 import utility.convert_csv as cc
 
 def number_of_spots(data):
-    p_spots = data[data['bydel'] == 'Indre By'].antal_pladser.sum() 
+    # Dataframe with all the parking spots for centrum.
+    p_spots_centrum = data[data['bydel'] == 'Indre By']
 
-    print('There are', p_spots, "parking spots in centrum") 
+    # We sum because there can be more than one parking spot in each place.
+    p_spots_total = p_spots_centrum.antal_pladser.sum() 
+
+    # Which road has most parkingspots.
+    index_most_parkingspots = p_spots_centrum['antal_pladser'].idxmax() #.vejnavn.unique()[newlist.index(max(newlist))]
+    road_name = p_spots_centrum['vejnavn'][index_most_parkingspots]
+    amount_p_spots = p_spots_centrum['antal_pladser'][index_most_parkingspots]
+    
+    print('There are', p_spots_total, "parking spots in centrum") 
+    print(road_name, 'is the road with most parkingspots and it has', amount_p_spots)
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ https://raw.githubusercontent.com/rmlassesen/dataset/master/p_pladser.csv
 import utility.downloader as download
 import utility.convert_csv as convert_csv
 import library.centrum_parking as ex1
-import library.privat_parking as privat_parking
+import library.privat_parking as ex5
 import library.public_and_privat_parking as ex3
 import library.marked_parking_spots as ex2
 
@@ -17,15 +17,6 @@ if __name__ == '__main__':
     file_name = download.download_file()
 
 data = convert_csv.convert_csv_to_dataframe(file_name)
-#cp.number_of_parking_spots(data)
-
-# Get the income data
-#data_income = dl.get_income_data()
-
-#data_income = cc.convert_income_to_dataframe([1, 5, 6, 7, 8])
-
-#data_park = cc.convert_p_space_to_dataframe([2, 3, 6, 7, 9])
-
 
 # Get the income file
 data_income_file = download.get_income_data()
@@ -37,5 +28,4 @@ ex1.number_of_spots(data)
 ex2.marked_spots(data)
 ex3.public_and_private_parking_spots(data)
 
-# opg 5
-#privat_parking.prinat_parking_electric_cars(data, income_data)
+ex5.privat_parking_electric_cars(data, income_data)
